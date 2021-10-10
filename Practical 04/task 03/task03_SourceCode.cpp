65d5581662a6abefdf6f94976087ab2a2dd72a19
Pramod Singh Rautela <91174359+PramodSinghRautela@users.noreply.github.com>
2021-10-10 16:19:36 +0530

    Create task03_SourceCode.cpp

---
+++ Practical 04/task 03/task03_SourceCode.cpp
@@ -0,0 +1,26 @@
+//Practical_04_Task03
+#include<iostream>
+using namespace std;
+namespace First{
+    int add(int a,int b)
+    {
+        return a+b;
+    }
+}
+namespace Second{
+    float add(float a,float b)
+    {
+        return a+b;
+    }
+}
+using namespace Second;
+int main()
+{
+    cout<<add(1,2)<<endl;
+    cout<<add(1.2,2)<<endl;
+    cout<<add(1,2.1)<<endl;
+    cout<<add(1.1,2.1)<<endl;
+    cout<<add(0,0)<<endl;
+   
+}
+
