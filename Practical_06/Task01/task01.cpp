d3097581b12fb2074b8167d8cc1b38855df798fb
Pramod Singh Rautela <91174359+PramodSinghRautela@users.noreply.github.com>
2021-10-22 08:17:31 +0530

    Add files via upload

---
+++ Practical_06/Task01/task01.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=2,b=6;
+    float c=9.36,d=5.25;
+    string s1="Hello";
+    string s2="World";
+    /*cout<<"int+int\n"<<(a+b)<<endl;
+    cout<<"float+float\n"<<(c+d)<<endl;
+    cout<<"float+int\n"<<(c+a)<<endl;
+    cout<<"string+int"<<s1+a;*/
+    cout<<"string+string\n"<<s1+s2<<endl;
+}
